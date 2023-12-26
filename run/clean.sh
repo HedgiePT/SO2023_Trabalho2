20a3b8f6c20631465e0c205d6a95a5cfee88a5a9
João Diogo Craveiro Ferreira <devilj@outlook.pt>
2023-12-26 18:21:52 +0000

    run/clean.sh: Properly clean shared memory and semaphores.

--- run/clean.sh
+++ run/clean.sh
@@ -3,7 +3,6 @@
 rm -f error*
 rm -f core
 
-# change 0x61066137 to your semaphore and shared memory key
-ipcrm -S 0x6106b0f5
-ipcrm -M 0x6106b0f5
-
+# change 0x611a48c7 to your semaphore and shared memory key
+ipcrm -S 0x611a48c7
+ipcrm -M 0x611a48c7
